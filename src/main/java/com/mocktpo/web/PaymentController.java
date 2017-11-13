a53eea53b43a789e4ae7543b03aceaf622020084
yangyifeng <cnyangyifeng@gmail.com>
2017-11-13 12:40:32 +0800

    n/a

--- src/main/java/com/mocktpo/web/PaymentController.java
+++ src/main/java/com/mocktpo/web/PaymentController.java
@@ -5,23 +5,16 @@ import com.mocktpo.modules.pay.AlipayService;
 import com.mocktpo.orm.domain.Order;
 import com.mocktpo.util.OrderHelper;
 import com.mocktpo.web.vo.OrderVo;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.PrintWriter;
-import java.util.Map;
 
 @Controller
 public class PaymentController {
-
-    private static final Logger logger = LogManager.getLogger();
 
     @Autowired
     private AlipayService alipayService;
@@ -48,41 +41,7 @@ public class PaymentController {
         }
     }
 
-    @RequestMapping(value = "/payments/alipay/response", method = RequestMethod.GET)
-    public ModelAndView alipayResponse(@RequestParam Map map) {
-        logger.debug("Callback request parameters from Alipay:");
-        logger.debug("app_id: {}", map.get("app_id"));
-        logger.debug("method: {}", map.get("method"));
-        logger.debug("sign_type: {}", map.get("sign_type"));
-        logger.debug("sign: {}", map.get("sign"));
-        logger.debug("charset: {}", map.get("charset"));
-        logger.debug("timestamp: {}", map.get("timestamp"));
-        logger.debug("version: {}", map.get("version"));
-        logger.debug("auth_app_id: {}", map.get("auth_app_id"));
-        logger.debug("out_trade_no: {}", map.get("out_trade_no"));
-        logger.debug("trade_no: {}", map.get("trade_no"));
-        logger.debug("total_amount: {}", map.get("total_amount"));
-        logger.debug("seller_id: {}", map.get("seller_id"));
-        ModelAndView mv = new ModelAndView();
-        String orderNumber = map.get("out_trade_no").toString();
-        Order order = orderService.findByOrderNumber(orderNumber);
-        if (order != null) {
-            orderService.complete(order);
-            mv.setViewName("redirect:/orders/" + orderNumber);
-        } else {
-            mv.setViewName("/error");
-        }
-        return mv;
-    }
-
     @RequestMapping(value = "/payments/paypal/request", method = RequestMethod.POST)
     public void paypalRequest() {
-    }
-
-    @RequestMapping(value = "/payments/paypal/response", method = RequestMethod.GET)
-    public ModelAndView paypalResponse(@RequestParam Map map) {
-        ModelAndView mv = new ModelAndView();
-        mv.setViewName("redirect:/orders/" + map.get("out_trade_no").toString());
-        return mv;
     }
 }
--- src/main/java/com/mocktpo/web/PaymentRestController.java
+++ src/main/java/com/mocktpo/web/PaymentRestController.java
@@ -2,17 +2,55 @@ package com.mocktpo.web;
 
 import com.mocktpo.modules.order.OrderService;
 import com.mocktpo.orm.domain.Order;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
 
 @RestController
 public class PaymentRestController {
 
+    private static final Logger logger = LogManager.getLogger();
+
     @Autowired
     private OrderService orderService;
+
+    @RequestMapping(value = "/api/v1/payments/alipay/response", method = RequestMethod.GET)
+    public ModelAndView alipayResponse(@RequestParam Map map) {
+        logger.info("Callback request parameters from Alipay:");
+        logger.info("app_id: {}", map.get("app_id"));
+        logger.info("method: {}", map.get("method"));
+        logger.info("sign_type: {}", map.get("sign_type"));
+        logger.info("sign: {}", map.get("sign"));
+        logger.info("charset: {}", map.get("charset"));
+        logger.info("timestamp: {}", map.get("timestamp"));
+        logger.info("version: {}", map.get("version"));
+        logger.info("auth_app_id: {}", map.get("auth_app_id"));
+        logger.info("out_trade_no: {}", map.get("out_trade_no"));
+        logger.info("trade_no: {}", map.get("trade_no"));
+        logger.info("total_amount: {}", map.get("total_amount"));
+        logger.info("seller_id: {}", map.get("seller_id"));
+        ModelAndView mv = new ModelAndView();
+        String orderNumber = map.get("out_trade_no").toString();
+        Order order = orderService.findByOrderNumber(orderNumber);
+        if (order != null) {
+            orderService.complete(order);
+            mv.setViewName("redirect:/orders/" + orderNumber + "/complete");
+        } else {
+            mv.setViewName("/error");
+        }
+        return mv;
+    }
+
+    @RequestMapping(value = "/api/v1/payments/paypal/response", method = RequestMethod.GET)
+    public ModelAndView paypalResponse(@RequestParam Map map) {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("redirect:/orders/" + map.get("out_trade_no").toString());
+        return mv;
+    }
 
     @RequestMapping(value = "/api/v1/payments/alipay/notify", method = RequestMethod.POST)
     @ResponseBody
